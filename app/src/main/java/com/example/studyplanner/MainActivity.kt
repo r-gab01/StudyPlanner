72d7ee0f6096fb991913cf7d31af2a84d9c71ba4
r-gab01 <137072372+r-gab01@users.noreply.github.com>
2023-06-19 15:49:56 +0200

    Prova

--- app/src/main/java/com/example/studyplanner/MainActivity.kt
+++ app/src/main/java/com/example/studyplanner/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ciao = "ciao"
+        println(ciao)
         setContentView(R.layout.activity_main)
     }
 }
