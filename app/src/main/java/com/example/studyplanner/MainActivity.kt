22041b7ad7661d43c9afe4a85d0204d627fecc39
r-gab01 <137072372+r-gab01@users.noreply.github.com>
2023-06-20 20:22:41 +0200

    ViewBinding

--- app/src/main/java/com/example/studyplanner/MainActivity.kt
+++ app/src/main/java/com/example/studyplanner/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val ciao = "ciao"
-        println(ciao)
         setContentView(R.layout.activity_main)
     }
 }
