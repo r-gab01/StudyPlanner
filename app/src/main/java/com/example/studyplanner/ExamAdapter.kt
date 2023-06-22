921629955b001c2c0f8462bdca22e532169bfa6f
r-gab01 <137072372+r-gab01@users.noreply.github.com>
2023-06-22 16:35:09 +0200

    5 bottoni nel menu e examFragment

---
+++ app/src/main/java/com/example/studyplanner/ExamAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.studyplanner
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studyplanner.databinding.ExamsCardViewBinding
+
+class ExamAdapter(private val examsList: List<ExamViewModel>) : RecyclerView.Adapter<ExamAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ExamsCardViewBinding): RecyclerView.ViewHolder(binding.root) {
+        val title = binding.titleCardView
+        val date = binding.dataEsameTextView
+        val remainingDays = binding.GiorniRimTextView
+        val image = binding.imageviewCard
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = ExamsCardViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val examViewModel = examsList[position]     //variabile contenente una examList ( ossia lista di examViewModel)
+        //prendo i valori dalla lista e li passo al viewHolder che li inserisce nelle CardView
+        if (examsList[position].tipo==0)        //se il tipo è 0 allora è un esame, altrimenti sono esercizi
+            holder.image.setImageResource(R.drawable.baseline_library_books_24)
+        else
+            holder.image.setImageResource(R.drawable.baseline_edit_note_24)
+        holder.date.text = examsList[position].date
+        holder.title.text = examsList[position].title
+        holder.remainingDays.text = examsList[position].remainingDays.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return examsList.size
+    }
+}
--- app/src/main/java/com/example/studyplanner/MainActivity.kt
+++ app/src/main/java/com/example/studyplanner/MainActivity.kt
@@ -16,17 +16,76 @@ class MainActivity : AppCompatActivity() {
         if (!loggedIn){                 //se l'utente non è loggato, lancio la schermata di login
             login()
         }
-
+        //Di default avvio il fragment del calendario
         val manager = supportFragmentManager
         val transaction = manager.beginTransaction()
-        transaction.add(R.id.fragmentContainerView, CalendarFragment())
+        val calendarTag = "CalendarFragment"
+        transaction.add(R.id.fragmentContainerView, CalendarFragment(), calendarTag)
         transaction.commit()
 
+        binding.bottomBar.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.calendar_button -> {
+                    val fragmentManager = supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    if (!fragmentExist(calendarTag)) {  //verifico se già il fragment è stato aperto tramite questa funzione definita sotto
+                        transaction.replace(R.id.fragmentContainerView, CalendarFragment(), calendarTag)
+                        transaction.commit()
+                    }
+                    true
+                }
+                R.id.exams_button ->{
+                    val fragmentManager = supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    val examsTag = "ExamsFragment"
+                    if (!fragmentExist(examsTag)) {  //verifico se già il fragment è stato aperto tramite questa funzione definita sotto
+                        transaction.replace(R.id.fragmentContainerView, ExamsFragment(), examsTag)
+                        transaction.commit()
+                    }
+                    true
+                }
+                R.id.stats_button ->{
+                    val fragmentManager = supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    val statsTag = "StatsFragment"
+                    if (!fragmentExist(calendarTag)) {  //verifico se già il fragment è stato aperto tramite questa funzione definita sotto
+                        transaction.replace(R.id.fragmentContainerView, CalendarFragment(), statsTag)
+                        transaction.commit()
+                    }
+                    true
+                }
+                R.id.studio_button ->{
+                    val fragmentManager = supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    val studioTag = "studioFragment"
+                    if (!fragmentExist(studioTag)) {  //verifico se già il fragment è stato aperto tramite questa funzione definita sotto
+                        transaction.replace(R.id.fragmentContainerView, CalendarFragment(), studioTag)
+                        transaction.commit()
+                    }
+                    true
+                }
+                R.id.profile_button ->{
+                    val fragmentManager = supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    val profileTag = "ProfileFragment"
+                    if (!fragmentExist(profileTag)) {  //verifico se già il fragment è stato aperto tramite questa funzione definita sotto
+                        transaction.replace(R.id.fragmentContainerView, CalendarFragment(), profileTag)
+                        transaction.commit()
+                    }
+                    true
+                }
+                else ->false
+            }
+        }
 
     }
 
+    fun fragmentExist(tag: String): Boolean {       //funzione che mi permette di trovare se un fragment è presente tramite il uso tag
+        val fragmentManager = supportFragmentManager
+        return (fragmentManager.findFragmentByTag(tag) != null)         //scrittura compatta che restituisce true o false se quella condizione si verifica o meno
+    }
 
-    private fun login(){
+    fun login(){
 
     }
 }
