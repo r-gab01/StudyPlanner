c5bd1bed915b9e017ac9373f29b96be4e39c0022
Saldiga01 <salvatoredigangi6@gmail.com>
2023-06-24 20:27:25 +0200

    Schermata aggiungi quasi completa

--- app/src/main/java/com/example/studyplanner/CalendarFragment.kt
+++ app/src/main/java/com/example/studyplanner/CalendarFragment.kt
@@ -1,23 +1,39 @@
 package com.example.studyplanner
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.studyplanner.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
 
+    private lateinit var binding: FragmentCalendarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        binding= FragmentCalendarBinding.inflate(inflater)
+
+        val ExamButton=binding.addExamButton
+
+        ExamButton.setOnClickListener{
+            val i= Intent(requireContext(),AddExamActivity:: class.java)
+            startActivity(i)
+        }
+
+        return binding.root
     }
+
+
 
 }
